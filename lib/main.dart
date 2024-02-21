bee93d0345a10058d59261c78663e66e1dd4e28f
Marcussha <hathong.work@gmail.com>
2024-02-21 21:05:37 +0700

    update to up database

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_c/screens/home.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Platform.isAndroid ? await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyAFBC9UlUHTFLIRWLIU6-E7d_3uO4x8MWM',
+        appId: '1:466691426632:android:12650232f2e9458b704b7d',
+        messagingSenderId: '466691426632',
+        projectId: 'radiant-micron-403105'
+    ))
+      :await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
